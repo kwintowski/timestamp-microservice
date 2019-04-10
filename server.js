4b1673bb77df198dc7827a19b6e3991bd92efc28
Andrew Kwintowski <39303397+kwintowski@users.noreply.github.com>
2019-04-10 23:05:35 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// timestamp endpoint
+app.get('/api/timestamp/:date_string', function(req,res,next){
+  var input = req.params.date_string;
+  var date = new Date(input);
+  
+  if(isNaN(date) && !isNaN(+input)) {
+    date = new Date(+input);
+  }
+  req.result = {unix:date.getTime(), utc:date.toUTCString()};
+  next();
+  }, 
+  function(req,res) {
+    res.send(req.result);
+  });
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
